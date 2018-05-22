6dca73ca41c140a527adac840afe3b3e6891c344
nsourov <naimur.rahman103@gmail.com>
2018-05-22 22:47:35 +0600

    make it better

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,10 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./keys_prod');
-} else {
-  module.exports = require('./keys_dev');
-}
+// if (process.env.NODE_ENV === 'production') {
+//   module.exports = require('./keys_prod');
+// } else {
+//   module.exports = require('./keys_dev');
+// }
+
+module.exports = {
+  mongoURI: 'mongodb://devconnection:devconnection@ds263989.mlab.com:63989/dev-connector',
+  secretOrKey: 'secret'
+};
--- server.js
+++ server.js
@@ -14,6 +14,26 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 // DB Config
 const db = require('./config/keys').mongoURI;
 
@@ -34,15 +54,11 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-// Server static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+// Express only serves static assets in production
+// if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+// }
+app.get('*', (req, res) => res.sendFile(path.resolve('client/build', 'index.html')));
 
 const port = process.env.PORT || 5000;
 
